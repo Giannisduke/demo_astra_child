8c7dbc0a6e07ce0f35677df808cf4e929abdeb4d
Giannisduke <admin@eboy.gr>
2021-06-07 11:08:19 +0300

    Create Page Template

--- functions.php
+++ functions.php
@@ -13,7 +13,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function load_custom_files() {
+	// load custom css
 	wp_register_style( 'custom_css', get_stylesheet_directory_uri(). '/assets/css/custom.css');
 	wp_enqueue_style( 'custom_css' );
+
+	// load custom js
+	wp_register_script('custom_js', get_stylesheet_directory_uri(). '/assets/js/custom.js');
+	wp_enqueue_script( 'custom_js' );
 }
 add_action ('wp_enqueue_scripts', 'load_custom_files', 10 );
+
+
+
+function custom_text() {
+
+}
+add_action('giannis_custom_fumction', 'custom_text');
---
+++ template_1.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Template Name: Page Template 1
+*/
+
+get_header(); ?>
+
+<?php the_content(); ?>
+
+<?php get_footer(); ?>
